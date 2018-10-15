c0f89077371e210559ef7f1c6cc5aa6c599d5f39
Wheeler Pryor <wheeler.pryor@gmail.com>
2018-10-15 17:07:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ function increaseRankBy(n) {
   const lis = document
     .getElementById('app')
     .querySelectorAll('ul.ranked-list li');
- 
+
   for (let i = 0; i < lis.length; i++) {
     lis[i].innerHTML = (i + 1).toString();
   }
