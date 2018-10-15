a867f38e21c9950a05c8946693b9a4be21858af5
Wheeler Pryor <wheeler.pryor@gmail.com>
2018-10-15 17:12:23 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
 
+function nestedTarget() {
+  return document.querySelector('#nested .target');
+}
+
+function deepestChild() {
+  let currentNode = document.querySelector('#grand-node');
+  while(currentNode.children.length !== 0) {
+    currentNode = currentNode.children[0];
+  }
+  return currentNode;
+}
+
+function increaseRankBy(n) {
+  const ranks = document.querySelectorAll('.ranked-list li');
+  for(let i = 0; i < ranks.length; i++) {
+    ranks[i].innerHTML = (parseInt(ranks[i].innerHTML) + n).toString();
+  }
+}
