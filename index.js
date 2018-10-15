a62602d8ebe0a090c32f4f3322d0279d4910604a
Wheeler Pryor <wheeler.pryor@gmail.com>
2018-10-15 17:07:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,9 +15,16 @@ function deepestChild() {
 }
 
 function increaseRankBy(n) {
-  let ranks = document.querySelectorAll('.ranked-list li');
-  console.log(ranks[0]);
-  ranks[0].innerHtml = 'test';
+  const lis = document
+    .getElementById('app')
+    .querySelectorAll('ul.ranked-list li');
+ 
+  for (let i = 0; i < lis.length; i++) {
+    lis[i].innerHTML = (i + 1).toString();
+  }
+  //const ranks = document.querySelectorAll('.ranked-list li');
+  //console.log(ranks[0]);
+  //ranks[0].innerHtml = 'test';
   //ranks.innerHtml = 'test';
   //for(let i = 0; i < ranks.length; i++) {
   //  (parseInt(ranks[i].innerHtml) + n).toString();
